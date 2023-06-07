96e405b7341802dd7f0faa753c989b5d93267ef0
Ye <ocean.yef@gmail.com>
2023-06-07 14:42:19 +0800

    更新录屏间隔

--- camera_open.py
+++ camera_open.py
@@ -22,6 +22,9 @@ fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 timer = 0
 interval = 2 * 60  # 2分钟
 
+out = None
+out_filename = ""
+
 while True:
     # 读取视频流的一帧
     ret, frame = cap.read()
@@ -39,39 +42,27 @@ while True:
 
     # 如果计时器达到保存间隔
     if timer >= interval:
+        # 释放之前的VideoWriter对象
+        if out is not None:
+            out.release()
+
         # 获取当前时间作为文件名
         current_time = datetime.datetime.now()
         filename = current_time.strftime("%Y-%m-%d-%H-%M-%S") + ".mp4"
 
-        # 创建VideoWriter对象，指定输出文件路径、编码器、帧率和分辨率
+        # 创建新的VideoWriter对象，指定输出文件路径、编码器、帧率和分辨率
         output_file = os.path.join(record_folder, filename)
         out = cv2.VideoWriter(output_file, fourcc, 30.0, (640, 480))
 
-        # 开始保存视频段落，直到计时器重新达到间隔
-        while timer < interval:
-            # 读取视频流的一帧
-            ret, frame = cap.read()
+        # 更新当前的输出文件名
+        out_filename = filename
 
-            if not ret:
-                break
+        # 重置计时器
+        timer = 0
 
-            # 在帧上进行一些处理，如果需要的话
-
-            # 将帧写入VideoWriter对象
-            out.write(frame)
-
-            # 显示处理后的帧
-            cv2.imshow('Frame', frame)
-
-            # 按下 'q' 键退出循环
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-            # 计时器增加帧间隔
-            timer += 1
-
-        # 释放保存视频段落的VideoWriter对象
-        out.release()
+    # 如果当前有VideoWriter对象，则将帧写入其中
+    if out is not None:
+        out.write(frame)
 
     # 按下 'q' 键退出循环
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -79,4 +70,9 @@ while True:
 
 # 释放资源
 cap.release()
+
+# 释放最后一个VideoWriter对象
+if out is not None:
+    out.release()
+
 cv2.destroyAllWindows()
