d3ff8390789c89766ea2bcae4aeb11ac4e921f6f
Ye <ocean.yef@gmail.com>
2023-06-15 09:48:48 +0800

    新增区域选定和FPS统计

--- camera_test.py
+++ camera_test.py
@@ -1,4 +1,7 @@
+import copy
 import math
+import threading
+import time
 
 import cv2
 import numpy as np
@@ -19,10 +22,11 @@ if camera_on:
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     cap.set(cv2.CAP_PROP_FPS, 10)
+    print("camera open")
 
 else:
     cap = cv2.VideoCapture('test.mp4')
-
+    print("video open")
 
 
 def get_angle(pt1, pt2):
@@ -74,8 +78,10 @@ def sort_coordinates(src_bound):
     bottommost = np.min(src_bound, axis=0)
     start_point = [leftmost[0], bottommost[1]]
 
+    mean_point = np.mean(src_bound, axis=0)
+
     # 按顺时针方向排序
-    sorted_indices = np.argsort(np.arctan2(src_bound[:, 0] - start_point[0], src_bound[:, 1] - start_point[1]))
+    sorted_indices = np.argsort(np.arctan2(src_bound[:, 0] - mean_point[0], src_bound[:, 1] - mean_point[1]))
     sorted_coordinates = src_bound[sorted_indices]
 
     # 转换为相对左下角的坐标
@@ -91,17 +97,20 @@ def sort_coordinates(src_bound):
 
         # 标记最终顺序
         for i, (x, y) in enumerate(relative_coordinates):
-            ax.text(x, y, str(i+1), va='center', ha='center')
+            ax.text(x+3, y+3, str(i+1), va='center', ha='center')
 
         plt.show()
+
+
+
+
+
 
     return relative_coordinates
 
 
 
-import cv2
-import numpy as np
-import pytesseract
+
 
 def ocr(image):
     # 将图像转换为灰度图像
@@ -116,11 +125,11 @@ def ocr(image):
     mask = np.all((image >= min_color) & (image <= max_color), axis=2)
 
     # 显示mask结果
-    cv2.imshow('Mask Image', mask.astype(np.uint8) * 255)
+    # cv2.imshow('Mask Image', mask.astype(np.uint8) * 255)
 
     # 等待用户按下空格键
-    while cv2.waitKey(0) != ord(' '):
-        pass
+    # while cv2.waitKey(0) != ord(' '):
+    #     pass
 
     # 对图像进行二值化处理
     _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
@@ -140,7 +149,7 @@ def ocr(image):
         roi = binary[y:y+h, x:x+w]
 
         # 使用Tesseract进行文字识别
-        result = pytesseract.image_to_string(roi, lang='chi_sim+eng')
+        result = pytesseract.image_to_string(roi, lang='eng')
 
         # 显示识别结果
         cv2.putText(image, result, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
@@ -157,6 +166,64 @@ def ocr(image):
 
     return result
 # 创建保存图片的文件夹
+
+def mouse_callback(event, x, y, flags, param):
+    global pmx, pmy
+    if event == cv2.EVENT_LBUTTONDOWN:  # 检测到鼠标左键点击事件
+        print(f"鼠标点击坐标：({x}, {y})")
+        global is_clicked
+        is_clicked = True
+        pmx = x
+        pmy = y
+
+
+def ocr_percent(image):
+    # 将图像转换为灰度图像
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # 使用Tesseract进行文字识别
+    result = pytesseract.image_to_string(gray, lang='eng')
+
+    # 显示识别结果
+    return result
+
+
+# 判定点是否在 contour 内部
+def is_point_inside_contour(contour, point):
+    # 将点转换为tuple格式
+    point_tuple = (point[0], point[1])
+
+    # 判定点与轮廓的关系
+    result = cv2.pointPolygonTest(contour, point_tuple, False)
+
+    # 返回结果
+    return result >= 0
+
+
+
+
+# 计时器类
+class FPSCounter:
+    def __init__(self):
+        self.start_time = None
+        self.true_count = 0
+
+    def start(self):
+        self.start_time = time.time()
+        self.true_count = 0
+
+    def update(self, value):
+        if value:
+            self.true_count += 1
+
+    def get_fps(self):
+        current_time = time.time()
+        elapsed_time = current_time - self.start_time
+        fps = self.true_count / elapsed_time if elapsed_time > 0 else 0
+        return fps
+
+
+#-------------------------------
 pic_folder = "pic_folder"
 if not os.path.exists(pic_folder):
     os.makedirs(pic_folder)
@@ -173,6 +240,40 @@ if not os.path.exists(clip_folder):
 
 
 
+ret, frame = cap.read()
+
+
+
+
+# 创建窗口并显示第一帧图像
+cv2.namedWindow('Video')
+frame = cv2.rotate(frame, cv2.ROTATE_180)
+cv2.imshow('Video', frame)
+
+
+pmx,pmy = 0,0
+# 设置鼠标回调函数
+cv2.setMouseCallback('Video', mouse_callback)
+
+
+# 等待鼠标点击或20秒等待期结束
+is_clicked = False
+timer = threading.Timer(20.0, lambda: None)
+timer.start()
+while not is_clicked and timer.is_alive():
+    if cv2.waitKey(1) == 27:  # 按下ESC键退出程序
+        break
+
+# 取消计时器
+timer.cancel()
+
+
+# 创建帧率统计器
+fps_counter = FPSCounter()
+
+# 循环执行 evaluate() 并统计帧率
+fps_counter.start()
+fps = 0
 
 while True:
     # 读取视频流的一帧
@@ -182,7 +283,8 @@ while True:
 
     if not ret:
         break
-
+    #保留原图
+    frame_copy = frame.copy()
     # 转换为灰度图像
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -190,7 +292,7 @@ while True:
     blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
 
     # 边缘检测
-    edges = cv2.Canny(blurred_frame, 100, 250)
+    edges = cv2.Canny(blurred_frame, 0, 100)
 
     # 寻找轮廓
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -201,13 +303,13 @@ while True:
         # 进行多边形逼近，将轮廓近似为较少的顶点
 
         hull = cv2.convexHull(contour)
-        approx = cv2.approxPolyDP(hull, 0.03 * cv2.arcLength(hull, True), True)
+        approx = cv2.approxPolyDP(hull, 0.1 * cv2.arcLength(hull, True), True)
 
         # 判断是否为非凹四边形
         if len(approx) == 4 and cv2.isContourConvex(approx):
 
 
-            print("---------"+str(count)+"-----------")
+            #print("---------"+str(count)+"-----------")
             parallel_approx=parallel_quar(approx,5)
 
             if(parallel_approx is None):
@@ -215,13 +317,13 @@ while True:
 
             area = cv2.contourArea(parallel_approx)
 
-            frame_shown = blurred_frame
+            frame_shown = frame
             #cv2.drawContours(frame, [hull], 0, (255, 0, 0), 2)
             # 判断面积是否大于等于20x20像素
-            if area >= 2000:
+            if area >= 5000 and area < 50000:
                 # 绘制轮廓
 
-                cv2.drawContours(frame_shown, [parallel_approx], 0, (0, 255, 0), 2)
+                cv2.drawContours(frame_shown, [parallel_approx], 0, (0, 240, 0), 2)
 
                 # 获取中心点坐标
                 M = cv2.moments(parallel_approx)
@@ -239,7 +341,7 @@ while True:
                 # 裁剪四边形区域并保存为图像文件
 
                 x, y, w, h = cv2.boundingRect(parallel_approx)
-                clip = frame[y:y + h, x:x + w]
+                clip = frame_copy[y:y + h, x:x + w]
                 #将x,y,w,h 生成dst_bound
                 dst_bound = np.float32([[0,0], [0,  h], [w, h], [w, 0]])
 
@@ -251,11 +353,27 @@ while True:
                 #print("dst_bound:",dst_bound)
                 transform_matrix = cv2.getPerspectiveTransform(src_bound,dst_bound )
                 clip_correct = cv2.warpPerspective(clip, transform_matrix, (w, h))
-                ocr_text = "none"#ocr(clip_correct)
-                #print(ocr_text)
-                cv2.imwrite(filepath+".png", clip)
-                cv2.imwrite(filepath+"-correct"+ocr_text+".png", clip_correct)
 
+                #cv2.imwrite(filepath + ".png", clip)
+                #cv2.imwrite(filepath + "-correct" + ".png", clip_correct)
+                #print("parallel_approx:",parallel_approx)
+                #print("pmx,pmy:",pmx,pmy)
+                in_contour = is_point_inside_contour(parallel_approx, (pmx, pmy))
+
+                if (pmx+pmy) == 0 or in_contour:
+                    #ocr_text = ocr_percent(clip_correct)
+                    ocr_text = ""
+                    print(ocr_text)
+
+                    cv2.imwrite(filepath+"-correct "+ocr_text+".png", clip_correct)
+
+                    cv2.drawContours(frame_shown, [parallel_approx], 0, ( 0,0, 255), 2)
+                    fps_counter.update(True)
+                    # 打印当前帧率
+                    fps = round(fps_counter.get_fps(),2)
+                    #print("帧率:", fps)
+
+                cv2.putText(frame_shown, "FPS:"+str(fps), (1000, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
                 count += 1
 
 
